b726069bee38f9df4b72cc32546e1b4922ebcc0f
Wayne <wayne.cen@gmail.com>
2023-07-25 17:07:13 -0400

    fix: Replace dev redirect uri with env variable

--- src/auth/authCodeWithPkce.ts
+++ src/auth/authCodeWithPkce.ts
@@ -27,7 +27,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
 	const params = new URLSearchParams()
 	params.append('client_id', clientId)
 	params.append('response_type', 'code')
-	params.append('redirect_uri', 'http://localhost:5173/callback')
+	params.append('redirect_uri', `${import.meta.env.VITE_REDIRECT_URI}`)
 	params.append('scope', 'user-read-private user-read-email')
 	params.append('code_challenge_method', 'S256')
 	params.append('code_challenge', challenge)
